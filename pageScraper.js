fc56ed7059ccedba05749e5449de5bf8193b5b63
wrmacdonald <wrmacdonald2016@gmail.com>
2023-04-10 11:56:18 -0700

    post route for user passed data works

--- pageScraper.js
+++ pageScraper.js
@@ -1,3 +1,11 @@
 const { scraperObjTruecar } = require("./lib/scraper");
 
-scraperObjTruecar(2, true);
+car_data = {
+  vin: "KNAFX4A61E5004372",
+  mileage: "50000",
+  zip: "97215",
+  color: "White",
+  manual: false,
+};
+
+scraperObjTruecar(car_data, true);
--- routes/quotes.js
+++ routes/quotes.js
@@ -3,36 +3,77 @@ const scraper = require("../lib/scraper");
 
 const router = express.Router();
 
+// GET route - used for test request with dummy data
 router.get("/", async (req, res, next) => {
-  // 1. Get the parameter "pages"
-  const { pages } = req.query;
+  // Dummy Data:
+  car_data = {
+    vin: "KNAFX4A61E5004372",
+    mileage: "50000",
+    zip: "97215",
+    color: "White",
+    manual: false,
+  };
 
-  // Get the body & validate it
-  // const { car_data } = req.body;
-
-  // test Github
-  // if (!car_data) {
-  //   res.status(400).json({
-  //     statusCode: 400,
-  //     message: "No Car Data Received",
-  //   });
-  // }
-
-  // 2. Call the scraper function
+  // Call the scraper function
   let quote_data;
   try {
-    quote_data = await scraper.scraperObjTruecar(pages, true); // set 2nd arg to false for Docker
+    quote_data = await scraper.scraperObjTruecar(car_data, true); // set 2nd arg to false for Docker
   } catch (error) {
     console.error(error);
     quote_data = -1;
   }
 
-  // 3. Return the array of questions to the client
+  // Return the array of questions to the client
   res.status(200).json({
     statusCode: 200,
     message: "Quotes correctly retrieved",
     data: { quote_data },
   });
+});
+
+// POST route - used for quotes from passed in user data
+router.post("/", async (req, res, next) => {
+  // Get the request body
+  const car_data = req.body;
+
+  if (!car_data) {
+    // No car data sent in body
+    res.status(400).json({
+      statusCode: 400,
+      message: "No Car Data Received",
+    });
+  } else {
+    // TODO: Validate body
+    console.log(car_data);
+
+    // Call the scraper function
+    let quote_data;
+    try {
+      quote_data = await scraper.scraperObjTruecar(car_data, true); // set 2nd arg to false for Docker
+
+      res.status(200).json({
+        statusCode: 200,
+        message: "Quotes correctly retrieved",
+        data: { quote_data },
+      });
+    } catch (error) {
+      console.error(error);
+      quote_data = -1;
+
+      res.status(500).json({
+        statusCode: 500,
+        message: "Error receiving quotes",
+        data: { quote_data },
+      });
+    }
+
+    // Return the array of questions to the client
+    // res.status(200).json({
+    //   statusCode: 200,
+    //   message: "Quotes correctly retrieved",
+    //   data: { quote_data },
+    // });
+  }
 });
 
 module.exports = router;
